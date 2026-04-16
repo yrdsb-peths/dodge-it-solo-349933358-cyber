80802ca0f4a0eb25cd6737c031a0443fb62954b2
Jason <baifnjdn@gmail.com>
2026-04-16 11:06:51 -0400

    Added basic elements into the game
    - Hero can move up and down.
    - The world generates bananas that move left.
    - The bananas deletes itself after hitting the wall.
    - The Hero can no longer move after touching a banana.

---
+++ Banana.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Banana here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Banana extends Actor
+{
+    /**
+     * Act - do whatever the Banana wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(-3);
+        if (getX() <= 0)
+        {
+            // setLocation(600, Greenfoot.getRandomNumber(400));
+            getWorld().removeObject(this);
+        }
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,17 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    private int time = 0;
     public MyWorld() {
         super(600, 400, 1);
+        addObject(new Hero(), 300, 200);
+        addObject(new Banana(), 600, Greenfoot.getRandomNumber(400));
+    }
+
+    public void act() {
+        time++;
+        if (time % 50 == 0) {
+             addObject(new Banana(), 600, Greenfoot.getRandomNumber(400));
+        }
     }
 }
